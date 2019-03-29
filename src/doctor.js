d94fce74ba4ab62da293b16c6c0428ad445efb5d
ThearyIm <thearyim@gmail.com>
2019-03-29 13:10:30 -0700

    update js files

--- src/doctor.js
+++ src/doctor.js
@@ -16,7 +16,24 @@ export default class DoctorLookup {
       request.send();
     });
 
-    getAllDoctors(doctor) {
+    getAllConditions(doctor) {
+      return new Promise(function(resolve, reject) {
+        let request = new XMLHttpRequest();
+        let url = `https://api.betterdoctor.com/2016-03-01/conditions?user_key=${process.env.exports.apiKey}`
+
+        request.onload = function() {
+          if (this.status === 200) {
+            resolve(request.response);
+          } else {
+            reject(Error(request.statusText));
+          }
+        }
+
+        request.open("GET", url, true);
+        request.send();
+      });
+
+    getDoctorByLocation(doctor) {
       return new Promise(function(resolve, reject) {
         let request = new XMLHttpRequest();
         let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${process.env.exports.apiKey}`
@@ -33,23 +50,6 @@ export default class DoctorLookup {
         request.send();
       });
 
-      getAllConditions(doctor) {
-        return new Promise(function(resolve, reject) {
-          let request = new XMLHttpRequest();
-          let url = `https://api.betterdoctor.com/2016-03-01/conditions?user_key=${process.env.exports.apiKey}`
-
-          request.onload = function() {
-            if (this.status === 200) {
-              resolve(request.response);
-            } else {
-              reject(Error(request.statusText));
-            }
-          }
-
-          request.open("GET", url, true);
-          request.send();
-        });
-  
     }
   }
 }
