cbdfe4f2aaaf13496c58733b656262684487138c
ThearyIm <thearyim@gmail.com>
2019-03-29 10:14:28 -0700

    Continue working on doctor.js

--- src/doctor.js
+++ src/doctor.js
@@ -32,7 +32,24 @@ export default class DoctorLookup {
         request.open("GET", url, true);
         request.send();
       });
-    }
 
+      getAllConditions(doctor) {
+        return new Promise(function(resolve, reject) {
+          let request = new XMLHttpRequest();
+          let url = `https://api.betterdoctor.com/2016-03-01/conditions?user_key=${process.env.exports.apiKey}`
+
+          request.onload = function() {
+            if (this.status === 200) {
+              resolve(request.response);
+            } else {
+              reject(Error(request.statusText));
+            }
+          }
+
+          request.open("GET", url, true);
+          request.send();
+        });
+  
+    }
   }
 }
