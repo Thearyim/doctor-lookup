356e90bb3ace45eddf712f97ed6b38030080b8ae
ThearyIm <thearyim@gmail.com>
2019-03-31 17:17:57 -0700

    Update html and search result

--- src/doctor.js
+++ src/doctor.js
@@ -4,7 +4,7 @@ export default class DoctorLookup {
       {
         return new Promise(function(resolve, reject) {
           let request = new XMLHttpRequest();
-          let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${process.env.exports.apiKey}`
+          let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=1&user_key=${process.env.exports.apiKey}`
 
           request.onload = function() {
             if (this.status === 200) {
--- src/main.js
+++ src/main.js
@@ -6,34 +6,86 @@ import DoctorLookup from "./doctor.js";
 
 
 $(document).ready(function() {
-  $('#searchForm').click(function(event) {
+  let condition;
+  let doctorName;
+
+  $('#searchForm').submit(function(event) {
     event.preventDefault();
-    let condition = $('#condition').val();
-    let doctorName = $('#doctorName').val();
+     condition = $('#condition').val();
+     doctorName = $('#doctorName').val();
 
-    showResults();
-  })
-});
-
-function showResults() {
-  let doctorLookup = new DoctorLookup();
-  let promise1 = doctorLookup.getDoctor(condition);
-  let promise2 = doctorLookup.getDoctor(doctorName);
-
-
-  promise1.then(function(response) {
-    let body = JSON.parse(response);
-    let doctorByCondition = body.main.profile;
-    console.log(doctorByCondition);
+     showResults();
   });
 
-  promise2.then(function(response) {
-    let body = JSON.parse(response);
-    let doctorByName = body.main.profile;
-  });
+  function filterByCondition(doctors, condition) {
+    let matchingDoctors = [];
+    for (let i = 0 ; i < doctors.length ; i++) {
+      let doctorSpecialty = doctors[i].specialties.description;
+      //if (doctorSpecialty.toLowerCase().contains(condition.toLowerCase())) {
+        matchingDoctors.push(doctors[i]);
+      //}
+    }
+    console.log(`doctors matching condition: ${matchingDoctors.length}`);
+    return matchingDoctors;
+  }
 
-  Promise.all([promise1, promise2]).then(function() {
-    $("#doctorByCondition").text(`Here is the list of doctors with specialty to treat your condition` + doctorByCondition);
-    $("#doctorByName").text("Here is the list of doctors with name you are searching" + doctorByName);
+  function showResults() {
+    let doctorLookup = new DoctorLookup();
+    console.log("executing 'getDoctor' API method");
+    let promise1 = doctorLookup.getDoctor(condition);
+
+    promise1.then(function(response) {
+      console.log("response received from 'getDoctor' API method");
+      console.log("parse response");
+      let body = JSON.parse(response);
+      console.log("response parsed successfully:");
+      let doctors = body.data;
+      doctors = filterByCondition(doctors, condition);
+      let html = '';
+
+      for (let i = 0 ; i < doctors.length ; i++) {
+        html += getDoctorDetailHtml(doctors[i]);
+      }
+      console.log(html);
+      $('#matchingDoctors').html(html);
+
     });
-}
+  }
+
+  function getDoctorDetailHtml(doctor) {
+    let practice = doctor.practices[0];
+    let html =
+    `<div>
+      <div> ${doctor.profile.first_name} ${doctor.profile.last_name}, ${doctor.profile.title}</div>
+      <div>${practice.name}</div>
+      <div>${practice.visit_address.street}<br/>
+        ${practice.visit_address.city}, ${practice.visit_address.state} ${practice.visit_address.zip}
+      </div>
+        <div>Accepts new patients:${practice.accepts_new_patients}</div>
+
+        <div>Website: <a href="${practice.website}">${practice.name}</a>
+        </div>
+    </div>`;
+    // <p>${doctor.profile.dynamic_bio}</p>
+    // <table>
+    //     <tr>
+    //
+    //         <td><a href="${data.attribution_url}" target="_new">${data.attribution_url}</a></td>
+    //     </tr>
+    //     <tr>
+    //         <th>Picture</th>
+    //         <td><img src='${doctor.profile.image_url}'></img></td>
+    //     </tr>
+    //     <tr>
+    //         <th>Specialties</th>
+    //         <td>
+    //         ${doctor.specialties}
+    //         ${name}<br>
+    //         ${doctor.specialties}
+    //         </td>
+    //     </tr>
+    // </table>`;
+
+    return html;
+  }
+});
